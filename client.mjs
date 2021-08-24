0ed12719323fc00e054f67f3266437d4825b6280
reesericci <reesericci@nyctech.cloud>
2021-08-24 09:57:59 -0500

    client via stream api

---
+++ client.mjs
@@ -0,0 +1,9 @@
+import { stdin } from 'process';
+import WebSocket, { createWebSocketStream } from 'ws';
+
+const ws = new WebSocket('ws://localhost:8080');
+
+const duplex = createWebSocketStream(ws, { encoding: 'utf8' });
+
+duplex.pipe(process.stdout);
+process.stdin.pipe(duplex);
