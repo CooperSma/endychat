2e7460b8d960ba8ffe3d62938c5da14ba0198a5c
reesericci <reesericci@nyctech.cloud>
2021-08-25 14:33:28 -0500

    color

--- server.mjs
+++ server.mjs
@@ -34,14 +34,14 @@ wss.on('connection', function connection(ws, req) {
   console.log(ws.name + ' has joined the chat')
   wss.clients.forEach(function each(client) { 
     if(client !== ws && client.readyState === WebSocket.OPEN) {
-      client.send(ws.name + ' has joined the chat' + '\n') 
+      client.send(chalk.green('--> ' + ws.name + ' has joined the chat') + '\n') 
     } else if(client === ws && client.readyState === WebSocket.OPEN) {
-      client.send("--> \x1b[32m You have joined the chat" + '\n')
+      client.send(chalk.green("--> You have joined the chat") + '\n')
     }
   });
   ws.on('close', function close(req) {
     wss.clients.forEach(function each(client) { 
-      client.send(ws.name + ' has left the chat' + '\n') 
+      client.send(chalk.red('--> ' + ws.name + ' has left the chat') + '\n') 
     });
   });
 });
