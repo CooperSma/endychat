78451622165b18e9fb3b798eee28a3efd25d2fd5
CoopGroup <46323207+CooperSma@users.noreply.github.com>
2021-08-25 11:05:35 -0500

    cool colors

--- server.mjs
+++ server.mjs
@@ -16,7 +16,7 @@ wss.on('connection', function connection(ws, req) {
     if(client !== ws && client.readyState === WebSocket.OPEN) {
       client.send(req.socket.remoteAddress + ' has joined the chat' + '\n') 
     } else if(client === ws && client.readyState === WebSocket.OPEN) {
-      client.send("You have joined the chat" + '\n')
+      client.send("--> \x1b[32m You have joined the chat" + '\n')
     }
   });
 });
