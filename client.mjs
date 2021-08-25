faaff5c734674325e74c9b146f8b86cd6e62baa6
CoopGroup <46323207+CooperSma@users.noreply.github.com>
2021-08-25 14:54:45 -0500

    connection error

--- client.mjs
+++ client.mjs
@@ -19,5 +19,9 @@ const ws = new WebSocket(process.env.WS_CLIENT_URL);
 
 const duplex = createWebSocketStream(ws, { encoding: 'utf8' });
 
+ws.on('close', function close(req) {
+    console.log(chalk.red("--> The remote server closed the connection"))
+    process.exit();
+});
 duplex.pipe(process.stdout);
-process.stdin.pipe(duplex);
+process.stdin.pipe(duplex);;
